75e3cb26ca6ea837e60ffdb63e2ed50e32d43d38
Ren <renchen8394@gmail.com>
2024-05-22 11:13:36 +0800

    feat: use iterabledataset

---
+++ datasets/noisy_ecg.py
@@ -0,0 +1,70 @@
+from pathlib import Path
+from torch.utils.data import IterableDataset
+
+import torch
+import numpy as np
+import wfdb
+
+ECG_LENGTH = 650000
+
+# noisy_ecg torch dataset
+class NOISY_ECG(IterableDataset):
+    def __init__(self, root="./data/physionet.org/files/", channel: list = [0], window_size: int = 1024, train:bool = True):
+        super(NOISY_ECG, self).__init__()
+        self.root = root
+        self.channel = channel
+        self.window_size = window_size
+        self.train = train
+
+        # MITDB subjects (w/o 118 and 119 who are in NSTDB)
+        mitdb_subjects = [s for s in wfdb.io.get_record_list("mitdb") if s not in ["118", "119"]]
+        self.val_subjects = np.random.choice(mitdb_subjects, 10)
+        self.train_subjects = [s for s in mitdb_subjects if s not in self.val_subjects]
+
+        # pure noise from NSTDB
+        self.bw, _ = wfdb.rdsamp(f"{root}/nstdb/1.0.0/bw", channels=channel) # baseline wander
+        self.em, _ = wfdb.rdsamp(f"{root}/nstdb/1.0.0/em", channels=channel) # electrode motion artifact
+        self.ma, _ = wfdb.rdsamp(f"{root}/nstdb/1.0.0/ma", channels=channel) # muscle artifact
+        self.noise_length = min(self.bw.shape[0], self.em.shape[0], self.ma.shape[0])
+
+    def __iter__(self):
+        while True:
+            random_subject = np.random.choice(self.train_subjects) if self.train else np.random.choice(self.val_subjects)
+            signal, _ = wfdb.rdsamp(f"{self.root}/mitdb/1.0.0/{random_subject}", channels=self.channel)
+
+            random_start = np.random.randint(0, ECG_LENGTH % self.window_size)
+            clean_signal = signal[random_start:random_start+self.window_size][:, 0]
+            noisy_signal = self.add_noise(clean_signal, np.random.choice(["bw", "em", "ma"]), np.random.randint(-6, 24), self.train)
+        
+            clean_signal = torch.from_numpy(clean_signal.reshape(1, self.window_size)).float()
+            noisy_signal = torch.from_numpy(noisy_signal.reshape(1, self.window_size)).float()
+
+            yield noisy_signal, clean_signal
+
+    def add_noise(self, signal, noise_type, snr, train=True):
+        assert noise_type in ["bw", "em", "ma"], "Noise type must be one of ['bw', 'em', 'ma']"
+
+        st = np.random.randint(0, self.noise_length-signal.shape[0]) if train else 0
+        if noise_type == "bw":
+            noise = self.bw[st:st+signal.shape[0], 0]
+        elif noise_type == "em":
+            noise = self.em[st:st+signal.shape[0], 0]
+        elif noise_type == "ma":
+            noise = self.ma[st:st+signal.shape[0], 0]
+        
+        # mix signal and noise with SNR
+        rms_clean = np.sqrt(np.mean(signal**2))
+        rms_noise = np.sqrt(np.mean(noise**2))
+        scale = rms_clean / (10**(snr / 20) * rms_noise)
+        noisy = signal + noise * scale
+
+        return noisy
+
+if __name__ == "__main__":
+    from torch.utils.data import DataLoader
+
+    noisy_ecg = NOISY_ECG()
+    dataloader = DataLoader(noisy_ecg, batch_size=4, num_workers=4)
+    for noisy, clean in dataloader:
+        print(noisy.shape, clean.shape)
+        break
--- train.py
+++ train.py
@@ -1,12 +1,15 @@
+import os
+
 from pathlib import Path
 from tqdm import tqdm, trange
-
+from sklearn.preprocessing import MinMaxScaler
 import numpy as np
-import os
 import torch
+from torch.utils.data import DataLoader
 import wfdb
 
 from models import FCN_DAE
+from datasets.noisy_ecg import NOISY_ECG
 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 
@@ -17,39 +20,11 @@ torch.backends.cudnn.benchmark = False
 # check and select device
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def add_noise(signal, noise_type, snr, train=True):
-    assert noise_type in ["bw", "em", "ma"], "Noise type must be one of ['bw', 'em', 'ma']"
-    st = np.random.randint(0, bw.shape[0]-signal.shape[0]) if train else 0
-    if noise_type == "bw":
-        noise = bw[st:st+signal.shape[0], 0]
-    elif noise_type == "em":
-        noise = em[st:st+signal.shape[0], 0]
-    elif noise_type == "ma":
-        noise = ma[st:st+signal.shape[0], 0]
-    
-    # mix signal and noise with SNR
-    rms_clean = np.sqrt(np.mean(signal**2))
-    rms_noise = np.sqrt(np.mean(noise**2))
-    scale = rms_clean / (10**(snr / 20) * rms_noise)
-    noisy = signal + noise * scale
-
-    return noisy
-
 if __name__ == "__main__":
-    # TODO: use generator to load data and make it more efficient
-
-    # preserve 118 and 119 for testing as they are in NSTDB
-    mitdb_subjects = [s for s in wfdb.io.get_record_list("mitdb") if s not in ["118", "119"]]
-    val_subjects = np.random.choice(mitdb_subjects, 10) # 10 subjects for validation
-    train_subjects = [s for s in mitdb_subjects if s not in val_subjects]
-
-    bw, _ = wfdb.rdsamp("./data/physionet.org/files/nstdb/1.0.0/bw", channels=[0])
-    em, _ = wfdb.rdsamp("./data/physionet.org/files/nstdb/1.0.0/em", channels=[0])
-    ma, _ = wfdb.rdsamp("./data/physionet.org/files/nstdb/1.0.0/ma", channels=[0])
-    
     # hyperparameters
     lr = 0.0001
-    epochs = 30
+    batch_size = 128
+    epochs = 64
 
     # load model
     model = FCN_DAE().to(DEVICE)
@@ -57,66 +32,48 @@ if __name__ == "__main__":
     optimizer = torch.optim.Adam(model.parameters(), lr)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.8)
 
+    # load dataset
+    train_dataloader = DataLoader(
+        NOISY_ECG(root="./data/physionet.org/files/", channel=[0], window_size=1024, train=True),
+        batch_size=batch_size, num_workers=4, pin_memory=True, shuffle=True,
+    )
+    val_dataloader = DataLoader(
+        NOISY_ECG(root="./data/physionet.org/files/", channel=[0], window_size=1024, train=False),
+        batch_size=batch_size, num_workers=4, pin_memory=True, shuffle=True,
+    )
+    max_iter_per_epoch = 1024
+
     best_loss = np.inf
-    train_loss = []
-    val_loss = []
+    average_train_loss = []
+    average_val_loss = []
     for epoch in trange(epochs, desc="FCN_DAE Training"):
         # train
         model.train()
-        for t, train_subject in tqdm(enumerate(train_subjects), desc=f"Train {epoch+1} Epoch", leave=False):
-            random_start = np.random.randint(0, 650000 % 1024)  # 650000 is the maximum length of the record, 1024 is the window size
-            signal, _ = wfdb.rdsamp(f"./data/physionet.org/files/mitdb/1.0.0/{train_subject}", channels=[0])   # only use first channel
-            
-            train_step_loss = 0
-            count = 0
-            for i in tqdm(range(random_start, 650000, 1024), desc="Step", leave=False):
-                clean_signal = signal[i:i+1024][:, 0]
-                if clean_signal.shape[0] != 1024:
-                    continue
-                noisy_signal = add_noise(clean_signal, "bw", np.random.randint(-6, 24))
-
-                clean_signal = torch.from_numpy(clean_signal.reshape(1, 1, 1024)).to(DEVICE)
-                noisy_signal = torch.from_numpy(noisy_signal.reshape(1, 1, 1024)).to(DEVICE)
-                
-                count += 1
-
-                optimizer.zero_grad()
-                output, _ = model(noisy_signal.float())
-                loss = criterion(output, clean_signal.float())
-                train_step_loss += loss.item()
-                loss.backward()
-                optimizer.step()
-            average_step_loss = train_step_loss / count
-            train_loss.append(average_step_loss)
+        train_loss = 0
+        for i, (noisy_signal, clean_signal) in tqdm(enumerate(train_dataloader), desc=f"Train {epoch+1} Epoch", leave=False):
+            optimizer.zero_grad()
+            output, _ = model(noisy_signal.to(DEVICE))
+            loss = criterion(output, clean_signal.to(DEVICE))
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item()
+            if i >= max_iter_per_epoch:
+                break
+        average_train_loss.append(train_loss / (i+1))
         scheduler.step()
 
         # validation
         model.eval()
-        current_val_loss = 0
-        for val_subject in tqdm(val_subjects, desc=f"Validate {epoch+1} Epoch", leave=False):
-            signal, _ = wfdb.rdsamp(f"./data/physionet.org/files/mitdb/1.0.0/{val_subject}", channels=[0])   # only use first channel
-            
-            val_step_loss = 0
-            count = 0
-            for j in tqdm(range(0, 650000, 1024), desc="Step", leave=False):
-                clean_signal = signal[j:j+1024][:, 0]
-                if clean_signal.shape[0] != 1024:
-                    continue
-                noisy_signal = add_noise(clean_signal, "bw", -6, train=False)
-
-                clean_signal = torch.from_numpy(clean_signal.reshape(1, 1, 1024)).to(DEVICE)
-                noisy_signal = torch.from_numpy(noisy_signal.reshape(1, 1, 1024)).to(DEVICE)
-                
-                count += 1
-
-                output, _ = model(noisy_signal.float())
-                loss = criterion(output, clean_signal.float())
-                val_step_loss += loss.item()
-            average_val_loss = val_step_loss / count
-            val_loss.append(average_val_loss)
-            current_val_loss += average_val_loss
-        
-        if current_val_loss < best_loss:
+        val_loss = 0
+        for i, (noisy_signal, clean_signal) in tqdm(enumerate(val_dataloader), desc=f"Validate {epoch+1} Epoch", leave=False):
+            output, _ = model(noisy_signal.to(DEVICE))
+            loss = criterion(output, clean_signal.to(DEVICE))
+            val_loss += loss.item()
+            if i >= max_iter_per_epoch:
+                break
+        average_val_loss.append(val_loss / (i+1))
+        # save best model
+        if (val_loss / (i+1)) < best_loss:
             best_loss = average_val_loss
             Path("./weights").mkdir(parents=True, exist_ok=True)
             torch.save(model.state_dict(), f"./weights/FCN_DAE_lr{str(lr).split('.')[-1]}.pth")
