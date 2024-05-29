feat: add normalisation during testing and fix wrong order

datasets/noisy_ecg.py
@@ -39,8 +39,8 @@ class NOISY_ECG(IterableDataset):
         
             # normalize signal
             scaler = MinMaxScaler()
-            clean_signal = scaler.fit_transform(clean_signal.reshape(-1, 1)).reshape(-1)
-            noisy_signal = scaler.transform(noisy_signal.reshape(-1, 1)).reshape(-1)
+            noisy_signal = scaler.fit_transform(noisy_signal.reshape(-1, 1)).reshape(-1)
+            clean_signal = scaler.transform(clean_signal.reshape(-1, 1)).reshape(-1)
 
             clean_signal = torch.from_numpy(clean_signal.reshape(1, self.window_size)).float()
             noisy_signal = torch.from_numpy(noisy_signal.reshape(1, self.window_size)).float()

test.py
@@ -1,9 +1,11 @@
 from pathlib import Path
-from tqdm import tqdm, trange
+import os
 
+from tqdm import tqdm, trange
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
 import matplotlib.pyplot as plt
 import numpy as np
-import os
 import torch
 import torch.nn.functional as F
 import wfdb
@@ -25,9 +27,16 @@ if __name__ == "__main__":
     clean_signal_118, _ = wfdb.rdsamp(f"./data/physionet.org/files/mitdb/1.0.0/118", channels=[0])   # only use first channel for 118
     clean_signal_119, _ = wfdb.rdsamp(f"./data/physionet.org/files/mitdb/1.0.0/119", channels=[0])   # only use first channel for 119
 
-    model = FCN_DAE().to(DEVICE)    # FCN_DAE
-    # model = BLSTM().to(DEVICE)      # BLSTM
-    model.load_state_dict(torch.load("./weights/FCN_DAE_lr0001.pth", map_location=DEVICE))
+    model_name = "FCN_DAE"
+    assert model_name in ["FCN_DAE", "BLSTM"], "Model name must be 'FCN_DAE' or 'BLSTM'"
+    if model_name == "FCN_DAE":
+        model = FCN_DAE(use_bn=False).to(DEVICE)    # FCN_DAE
+        ckpt_path = "./weights/FCN_DAE/lr0001_b64_e64.pth"
+        model.load_state_dict(torch.load(ckpt_path, map_location=DEVICE), strict=False)
+    elif model_name == "BLSTM":
+        model = BLSTM().to(DEVICE)
+        ckpt_path = "./weights/BLSTM/lr001_b64_e64.pth"
+        model.load_state_dict(torch.load(ckpt_path, map_location=DEVICE), strict=False)
 
     # test model and visualize results
     fig, ax = plt.subplots(1, 1, figsize=(10, 6))
@@ -36,17 +45,21 @@ if __name__ == "__main__":
         signal, _ = wfdb.rdsamp(f"./data/physionet.org/files/nstdb/1.0.0/{subject}", channels=[0])
         target_signal = clean_signal_118 if "118" in subject else clean_signal_119
 
-        for j in tqdm(range(0, 650000, 1024), desc="Step", leave=False):
+        for j in tqdm(range(0, 650000, 1024), desc=f"Step {j}", leave=False):
             noisy_signal = signal[j:j+1024][:, 0]
             clean_signal = target_signal[j:j+1024][:, 0]
 
             if clean_signal.shape[0] != 1024 or noisy_signal.shape[0] != 1024:
                     continue
+            
+            noisy_signal = scaler.fit_transform(noisy_signal.reshape(-1, 1)).reshape(-1)
+            clean_signal = scaler.transform(clean_signal.reshape(-1, 1)).reshape(-1)
 
-            noisy_signal = torch.from_numpy(noisy_signal.reshape(1, 1, 1024)).to(DEVICE)
-            clean_signal = torch.from_numpy(clean_signal.reshape(1, 1, 1024)).to(DEVICE)
+            clean_signal = torch.from_numpy(clean_signal.reshape(1, 1024)).float().to(DEVICE)
+            noisy_signal = torch.from_numpy(noisy_signal.reshape(1, 1024)).float().to(DEVICE)
 
             denoised_signal, _ = model(noisy_signal.float())
+            denoised_signal = denoised_signal.squeeze(0)
             mse = F.mse_loss(denoised_signal, clean_signal.float())
             mse_list.append(mse.item())
 
@@ -60,7 +73,7 @@ if __name__ == "__main__":
             ax.set_title(f"{subject} - MSE: {mse.item()}")
             ax.legend()
             plt.tight_layout()
-            Path(f"./results/{subject}").mkdir(parents=True, exist_ok=True)
-            plt.savefig(f"./results/{subject}/Step_{j}.png")
+            Path(f"./results/{model_name}/{Path(ckpt_path).stem}/{subject}").mkdir(parents=True, exist_ok=True)
+            plt.savefig(f"./results/{model_name}/{Path(ckpt_path).stem}/{subject}/Step_{j}.png")
 
     print(f"Mean MSE: {np.mean(mse_list)}")